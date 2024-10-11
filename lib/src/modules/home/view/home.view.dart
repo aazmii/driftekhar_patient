8e7653e704b2c5cda879a89f1bb0af7b48a8d015
Anas <anasmj7@gmail.com>
2024-10-12 04:13:52 +0600

    launched

---
+++ lib/src/modules/home/view/home.view.dart
@@ -0,0 +1,22 @@
+import 'package:doc_appointment/src/l10n/localization.dart';
+import 'package:flutter/material.dart';
+
+import '../../router/provider/route.provider.dart';
+import '../../setting/view/setting.view.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async => await fadePush(context, const SettingView()),
+          child: Text(t!.setting),
+        ),
+      ),
+    );
+  }
+}
