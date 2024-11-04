239d6a99d13007c04259a6baee0afd2b7f62fdce
Anas <anasmj7@gmail.com>
2024-11-04 22:18:01 +0600

    patient home 3

--- lib/src/modules/patient.modules/components/home.container/home.container.dart
+++ lib/src/modules/patient.modules/components/home.container/home.container.dart
@@ -34,7 +34,7 @@ class HomeContainer extends StatelessWidget {
               option.title,
               style: context.text.titleMedium!.copyWith(
                 fontWeight: FontWeight.bold,
-                color: context.theme.primaryColor,
+                color: context.theme.secondaryHeaderColor,
               ),
             ),
           ),
@@ -62,59 +62,61 @@ class FrostedGlassBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(30),
-      child: Container(
-        width: width,
-        height: height,
-        decoration: const BoxDecoration(
-          color: Colors.transparent,
-        ),
-        //we use Stack(); because we want the effects be on top of each other,
-        //  just like layer in photoshop.
-        child: Stack(
-          children: [
-            //blur effect ==> the third layer of stack
-            BackdropFilter(
-              filter: ImageFilter.blur(
-                //sigmaX is the Horizontal blur
-                sigmaX: 4.0,
-                //sigmaY is the Vertical blur
-                sigmaY: 10.0,
+    return RepaintBoundary(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: Container(
+          width: width,
+          height: height,
+          decoration: const BoxDecoration(
+            color: Colors.transparent,
+          ),
+          //we use Stack(); because we want the effects be on top of each other,
+          //  just like layer in photoshop.
+          child: Stack(
+            children: [
+              //blur effect ==> the third layer of stack
+              BackdropFilter(
+                filter: ImageFilter.blur(
+                  //sigmaX is the Horizontal blur
+                  sigmaX: 4.0,
+                  //sigmaY is the Vertical blur
+                  sigmaY: 10.0,
+                ),
+                //we use this container to scale up the blur effect to fit its
+                //  parent, without this container the blur effect doesn't appear.
+                child: Container(),
               ),
-              //we use this container to scale up the blur effect to fit its
-              //  parent, without this container the blur effect doesn't appear.
-              child: Container(),
-            ),
-            //gradient effect ==> the second layer of stack
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-
-                border: borderWidth == null
-                    ? null
-                    : Border.all(
-                        color: borderColor!,
-                        width: borderWidth ?? 1,
-                        // color: context.theme.primaryColor,
-                        // width: 2,
-                      ),
-
-                // border: Border.all(color: Colors.white.withOpacity(0.13)),
-                gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      //begin color
-                      Colors.white.withOpacity(0.15),
-                      //end color
-                      Colors.white.withOpacity(0.05),
-                    ]),
+              //gradient effect ==> the second layer of stack
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+      
+                  border: borderWidth == null
+                      ? null
+                      : Border.all(
+                          color: borderColor!,
+                          width: borderWidth ?? 1,
+                          // color: context.theme.primaryColor,
+                          // width: 2,
+                        ),
+      
+                  // border: Border.all(color: Colors.white.withOpacity(0.13)),
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        //begin color
+                        Colors.white.withOpacity(0.15),
+                        //end color
+                        Colors.white.withOpacity(0.05),
+                      ]),
+                ),
               ),
-            ),
-            //child ==> the first/top layer of stack
-            Center(child: child),
-          ],
+              //child ==> the first/top layer of stack
+              Center(child: child),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/src/modules/patient.modules/home/view/patient.home.3.dart
@@ -0,0 +1,227 @@
+import 'package:doc_appointment/src/constants/constants.dart';
+import 'package:doc_appointment/src/extensions/extensions.dart';
+import 'package:doc_appointment/src/modules/create.appointment/view/create.appointment.dart';
+import 'package:doc_appointment/src/modules/patient.modules/chembers/view/chembers.page.dart';
+import 'package:doc_appointment/src/modules/patient.modules/components/socila.icons.dart';
+import 'package:doc_appointment/src/modules/patient.modules/home/models/welcome.options.dart';
+import 'package:doc_appointment/src/modules/patient.modules/online.consultation/view/online.consultation.dart';
+import 'package:doc_appointment/src/modules/patient.modules/services/view/services.dart';
+import 'package:doc_appointment/src/modules/router/provider/route.provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../components/home.container/home.container.dart';
+import 'components/auto.scroll.text.dart';
+import 'components/surgon.carousel.dart';
+
+class PatientHome3 extends StatelessWidget {
+  const PatientHome3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final int crossAxisCount = context.width > 600 ? 4 : 2;
+    // return Scaffold(
+    //   body: Image.asset('assets/images/jpg/surgery4.jpg'),
+    // );
+    return Stack(
+      children: [
+        // Scaffold(body: SurgonCarousel()),
+        Scaffold(
+          // endDrawer: const AppDrawer(),
+          backgroundColor: primaryColor,
+
+          body: Column(
+            children: [
+              Stack(
+                clipBehavior: Clip.none,
+                // alignment: Alignment.topCenter,
+                children: [
+                  Container(
+                    color: primaryColor,
+                    height: 120,
+                    child: SurgonCarousel(),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 12.0),
+                      child: Image.asset(
+                        'assets/images/png/dr.iftekhar.png',
+                        color: Colors.white,
+                        // 'assets/images/png/logo.png',
+                        height: 40,
+                      ),
+                    ),
+                  ),
+                  const Positioned(bottom: 0, right: 0, child: SocialIcons()),
+                  Positioned(
+                    // top: 8,
+                    bottom: -50,
+                    left: 10,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      // borderRadius: BorderRadius.circular(100),
+                      child: Image.network(
+                        height: 100,
+                        width: 100,
+                        'https://driftekharalam.com/wp-content/uploads/2024/09/IF.png',
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) =>
+                            const Icon(Icons.error),
+                        loadingBuilder: (context, child, loadingProgress) =>
+                            loadingProgress == null
+                                ? child
+                                : const CircularProgressIndicator(),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: -80,
+                    left: 120,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Asst Prof. Dr. Mohammad Iftekhar Alam',
+                          style: context.text.titleLarge!.copyWith(
+                            color: context.theme.secondaryHeaderColor,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'Assistant Professor (Orthopaedic Surgery)​',
+                          style: context.text.titleMedium!.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'Dr.Sirajul Islam Medical College,Dhaka.​',
+                          style: context.text.titleMedium!.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              80.toHeight,
+              SizedBox(
+                width: context.width * 0.9,
+                child: ScrollingText(
+                    height: 50,
+                    text:
+                        'পেশেন্টদের ডায়াগনোসিস করে রোগ নির্ণয় করতে হয় ।প্রাথমিক ডায়াগনোসিস, রিপোর্ট দেখানো, প্রেসক্রিপশন সহ নানান সুবিধা উপভোগ করুন অল্প সময়ে, অল্প খরচে ',
+                    textStyle: context.text.titleLarge!.copyWith(
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+              // Text(
+              //   'Welcome!',
+              //   style: context.text.titleLarge!.copyWith(
+              //     color: context.theme.primaryColor,
+              //     fontWeight: FontWeight.bold,
+              //   ),
+              // ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: crossAxisCount,
+                      mainAxisSpacing: 30,
+                      crossAxisSpacing: 10,
+                    ),
+                    itemBuilder: (_, index) {
+                      return InkWell(
+                        onTap: () async => handleRoute(context, index),
+                        child: HomeContainer(
+                          option: _welcomeOptions[index],
+                        ),
+                      );
+                    },
+                    itemCount: _welcomeOptions.length,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // bottomNavigationBar: const PayAndConfimButton(),
+          floatingActionButton: IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              'assets/icons/whatsapp.svg',
+              height: 44,
+            ),
+            // icon: const Icon(
+            //   FontAwesomeIcons.whatsapp,
+            //   color: Colors.green,
+            //   size: 34,
+            // ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  handleRoute(BuildContext context, int index) async {
+    final service = _welcomeOptions[index].title;
+
+    if (service == 'Book Appointment') {
+      await fadePush(
+        context,
+        ChembersPage(
+          title: 'Select Chember',
+          onSelectChember: (chember) async => fadePush(
+            context,
+            CreateAppointment(
+              chember: chember,
+            ),
+          ),
+        ),
+      );
+    }
+    if (service == 'Online Consultation') {
+      await fadePush(context, const OnlineConsultationPage());
+    }
+    if (service == 'Chembers') {
+      await fadePush(context, const ChembersPage(title: 'Chembers'));
+    }
+    if (service == 'Services') {
+      await fadePush(context, const ServicesPage());
+    }
+  }
+}
+
+final _welcomeOptions = [
+  WelcomeOption(
+    title: 'Book Appointment',
+    image: 'assets/images/png/book.appointment.png',
+  ),
+  WelcomeOption(
+    title: 'Online Consultation',
+    // image: 'assets/images/png/online.consult.png',
+    image: 'assets/images/png/online.appointment.png',
+  ),
+  WelcomeOption(
+    title: 'Services',
+    image: 'assets/images/png/medical.kit.png',
+  ),
+  WelcomeOption(
+    title: 'Chembers',
+    image: 'assets/images/png/consult.png',
+  ),
+  WelcomeOption(
+    title: 'Socials',
+    image: 'assets/images/png/socials.png',
+  ),
+];
+
+class CarouseBackground extends StatelessWidget {
+  const CarouseBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
